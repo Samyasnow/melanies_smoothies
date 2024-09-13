15f546516f346f8f017b4607bdb50287e3d85274
Samyasnow <samya.maktite@gmail.com>
2024-09-13 16:03:57 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,6 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 #st.dataframe(data=my_dataframe, use_container_width=true)
 #st.stop()
 
-
 pd_df=my_dataframe.to_pandas()
 st.dataframe(pd_df)
 st.stop()
@@ -48,5 +47,5 @@ if ingredients_list:
     
     time_to_insert = st.button('Submit Order')
         if time_to_insert:
-        session.sql(my_insert_stmt).collect()
+        #session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, '+name_on_order+' !', icon="✅")
